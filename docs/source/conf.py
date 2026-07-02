946b78f73d839b8c5ef98eeda715c4dbcde5b4b2
Alex Stewart <alex.stewart@emerson.com>
2026-07-02 16:24:10 -0400

    Set word-break to manual
    The current HTML output will set 'word-break: auto' which will direct
    the browser to "intelligently" break words with a hyphen across
    newlines. We'd prefer that it not. So set it to 'manual' in a custom css
    override, so that it will only break if the word itself contains a
    hyphen at that place.
    
    Signed-off-by: Alex Stewart <alex.stewart@emerson.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -49,10 +49,14 @@ exclude_patterns = []
 #
 html_theme = 'bizstyle'
 
+html_css_files = [
+    "custom.css",
+]
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
+html_static_path = ["_static"]
 
 html_context = {
     'github_repo_url': 'https://github.com/ni/nilrt-docs/',
